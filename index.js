404eaef8881d4aee6045fbd07267385218a07d61
JAMES CARVALLYO <jpcarvallyo@gmail.com>
2023-08-13 15:19:08 -0700

    Add start script and change first readline question

--- index.js
+++ index.js
@@ -15,23 +15,20 @@ const rl = readline.createInterface({
 });
 
 async function main() {
-  await rl.question("Enter the root directory: ", async (rootDirectory) => {
-    const duplicates = await searchDuplicates(cleanDirPath(rootDirectory));
-    const dupMap = gatherDuplicates(duplicates);
+  await rl.question(
+    "Enter the directory to start search at: ",
+    async (rootDirectory) => {
+      const duplicates = await searchDuplicates(cleanDirPath(rootDirectory));
+      const dupMap = gatherDuplicates(duplicates);
 
-    await rl.question("Enter the output file path: ", async (outputPath) => {
-      await rl.question("Enter name of file: ", async (fileName) => {
-        await processFile(dupMap, cleanDirPath(outputPath), fileName);
-        rl.close();
+      await rl.question("Enter the output file path: ", async (outputPath) => {
+        await rl.question("Enter name of file: ", async (fileName) => {
+          await processFile(dupMap, cleanDirPath(outputPath), fileName);
+          rl.close();
+        });
       });
-    });
-  });
-
-  //   const passThroughStream = new PassThrough();
-  //   const readStream = fs.createReadStream(__dirname + "/duplicates.json");
-  //   readStream.pipe(passThroughStream);
-
-  //   passThroughStream.pipe(process.stdout);
+    }
+  );
 }
 
 main();
